9175dfcbbbad9cf468197ae9e9c7d729938612a0
bentagtow <94013701+bentagtow@users.noreply.github.com>
2021-12-03 11:35:02 -0600

    connects loginservlet to JSP file

--- src/main/java/com/in28minutes/LoginServlet.java
+++ src/main/java/com/in28minutes/LoginServlet.java
@@ -1,26 +1,22 @@
 package com.in28minutes;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(urlPatterns = "/login.do")
-public class LoginServlet extends HttpServlet{
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
-			throws ServletException, IOException 
-	{
-		PrintWriter writer = response.getWriter();
-		writer.println("Something cool!");
-		writer.println("Something coolER!");
-	
-		
-	}
+public class LoginServlet extends HttpServlet {
 
-	
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(request, response);
+
+
+    }
+
+
 }
--- src/main/webapp/WEB-INF/views/login.jsp
+++ src/main/webapp/WEB-INF/views/login.jsp
@@ -2,15 +2,15 @@
   Created by IntelliJ IDEA.
   User: bentagtow
   Date: 12/3/21
-  Time: 11:13 AM
+  Time: 11:33 AM
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>Title</title>
+    <title>My first JSP</title>
 </head>
 <body>
-
+<h1>Just call me JSP queen</h1>
 </body>
 </html>
