0ef57cd51753d61fec4cfef5818f1868f217de90
bentagtow <94013701+bentagtow@users.noreply.github.com>
2021-12-03 10:21:04 -0600

    removes example servlet, adds custom login servlet

---
+++ src/main/java/com/in28minutes/LoginServlet.java
@@ -0,0 +1,9 @@
+package com.in28minutes;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(urlPatterns = "/login.do")
+public class LoginServlet extends HttpServlet{
+	
+}
