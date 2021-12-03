7d3ad05b46996215617135fe6dadcd524f3d8eb1
bentagtow <94013701+bentagtow@users.noreply.github.com>
2021-12-03 17:43:06 -0600

    removes login service

--- src/main/java/com/in28minutes/springmvc/LoginController.java
+++ src/main/java/com/in28minutes/springmvc/LoginController.java
@@ -6,7 +6,6 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController {
@@ -20,6 +19,11 @@ public class LoginController {
     @RequestMapping(value="/login", method = RequestMethod.POST)
     public String handleLoginRequest(@RequestParam String name,
                                      @RequestParam String password, ModelMap model){
+        if(!service.validateUser(name, password)){
+            model.put("errorMessage", "invalid creds yo");
+            return "login";
+
+        }
         model.put("name", name);
         model.put("password", password);
         return "welcome";
